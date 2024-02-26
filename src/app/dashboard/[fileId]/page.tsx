76daea2fa3086d4007fc28b2ce113bd6ddba7c6f
Hakan Okay <hokay.ca@gmail.com>
2024-02-26 14:17:44 -0500

    File view init

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,5 +1,7 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 
 type FileIdProps = {
@@ -26,5 +28,21 @@ export default async function FileId({ params }: FileIdProps) {
 
   if (!file) notFound();
 
-  return <div>{fileId}</div>;
+  return (
+    <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
+      <div className="max-w-8xl mx-auto w-full grow lg:flex xl:px-2">
+        {/* left side */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 }
