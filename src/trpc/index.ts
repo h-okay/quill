8e5f33f5e71668bf251eea65436188afa86c8121
Hakan Okay <hokay.ca@gmail.com>
2024-02-25 22:28:05 -0500

    Dashboard base functionality

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,11 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { procedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from "zod";
 
 export const appRouter = router({
-  authCallback: procedure.query(async () => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
@@ -33,6 +34,32 @@ export const appRouter = router({
 
     return { success: true };
   }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const file = await db.file.findFirst({
+        where: { id: input.id, userId },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 // export type definition of API
