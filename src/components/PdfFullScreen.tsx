f962e899cd8415f4e6652b95131d28bc6c0549ec
Hakan Okay <hokay.ca@gmail.com>
2024-02-26 17:29:53 -0500

    PdfRenderer main functionality done

---
+++ src/components/PdfFullScreen.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Expand, Loader2 } from "lucide-react";
+import { DialogContent } from "./ui/dialog";
+import SimpleBar from "simplebar-react";
+import { Document, Page } from "react-pdf";
+import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
+
+type PdfFullScreenProps = {
+  fileUrl: string;
+};
+
+export default function PdfFullScreen({ fileUrl }: PdfFullScreenProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [numPages, setNumPages] = useState<number>();
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button variant="ghost" className="gap-1.5" aria-label="fullscreen">
+          <Expand className="h-4 w-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-full max-w-7xl">
+        <SimpleBar autoHide={false} className="mt-6 max-h-[calc(100vh-10rem)]">
+          <div ref={ref}>
+            <Document
+              file={fileUrl}
+              className="max-h-full"
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+            >
+              {new Array(numPages).fill(0).map((_, i) => (
+                <Page key={i} width={width ? width : 1} pageNumber={i + 1} />
+              ))}
+            </Document>
+          </div>
+        </SimpleBar>
+      </DialogContent>
+    </Dialog>
+  );
+}
