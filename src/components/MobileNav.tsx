56db4403f712e93ce1cb384a8f635a0f40c9ad14
Hakan Okay <hokay.ca@gmail.com>
2024-03-03 15:44:59 -0500

    Bigger mobile menu handle

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -30,9 +30,8 @@ export default function MobileNav({ isAuth, isPro }: MobileNavProps) {
     <div className="sm:hidden">
       <Menu
         onClick={toggleOpen}
-        className="relative z-50 h-5 w-5 text-zinc-700"
+        className="relative z-50 h-8 w-h-8 text-zinc-700"
       />
-
       {isOpen ? (
         <div className="fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-0 w-full">
           <ul className="absolute bg-white border-b border-zinc-200 shadow-xl grid w-full gap-3 px-10 pt-20 pb-8">
